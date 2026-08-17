d5f54e41db5ed292c44fa33e2d49967be81878a9
iTech <iTech@DESKTOP-RT7KEOQ>
2026-08-18 01:12:42 +0300

    Question02

--- OOP Assignment05/Program.cs
+++ OOP Assignment05/Program.cs
@@ -35,7 +35,31 @@
             #endregion
 
 
+            #region Question02
 
+            /// 02) Abstract Classes vs. Interfaces
+            ///
+            /// a) What is the difference between an Abstract Class and an Interface?
+            ///
+            /// Answer:
+            /// An abstract class can contain fields, properties, constructors,
+            /// normal methods, and abstract methods.
+            ///
+            /// An interface mainly defines a contract that specifies what members
+            /// a class must implement.
+            ///
+            /// An abstract class is used when classes share common data and behavior,
+            /// while an interface is used to define common capabilities or behaviors.
+            ///
+            /// b) When would you choose an Interface instead of an Abstract Class?
+            ///
+            /// Answer:
+            /// We choose an Interface when different classes need to follow the same
+            /// contract or provide the same behavior, even if they do not belong
+            /// to the same inheritance hierarchy.
+            ///
+           
+            #endregion
 
 
 
