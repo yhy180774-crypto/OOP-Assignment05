88873f29debcfead2f06da6dd1db6ea3174091d5
iTech <iTech@DESKTOP-RT7KEOQ>
2026-08-18 01:09:39 +0300

    Question01

--- OOP Assignment05/Program.cs
+++ OOP Assignment05/Program.cs
@@ -4,7 +4,143 @@
     {
         static void Main(string[] args)
         {
-            
+
+            #region Question01
+
+            /// 01) Abstraction
+            ///
+            /// a) What is Abstraction in Object-Oriented Programming?
+            ///
+            /// Answer:
+            /// Abstraction is the process of hiding unnecessary implementation details
+            /// and showing only the important features of an object.
+            ///
+            /// Example:
+            ///
+            /// shipment.PrintShipment();
+            ///
+            /// // The user calls PrintShipment() without knowing the internal
+            /// // implementation details of the method.
+            ///
+            /// b) Why is abstraction considered one of the four pillars of OOP?
+            ///
+            /// Answer:
+            /// Abstraction is one of the four pillars of OOP because it helps hide
+            /// unnecessary implementation details and allows the programmer to focus
+            /// on what an object does instead of how it does it.
+            ///
+            /// The four pillars of OOP are:
+            /// Encapsulation, Inheritance, Polymorphism, and Abstraction.
+
+            #endregion
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
         }
